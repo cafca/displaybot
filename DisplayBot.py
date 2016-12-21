fce165815f21a977135d2d5eedf808c8ca6050d7
Vincent Ahrend <nichte@gmail.com>
2016-12-21 02:22:58 +0100

    Remove faststart optioN

--- DisplayBot.py
+++ DisplayBot.py
@@ -187,7 +187,7 @@ def convert_gif(fpath):
 
     ff = ffmpy.FFmpeg(
         inputs={fpath: None},
-        outputs={new_fpath: '-movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"'}
+        outputs={new_fpath: '-pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"'}
     )
     ff.run()
     return new_fpath
