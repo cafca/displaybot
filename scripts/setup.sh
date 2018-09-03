e4e517525553b0ee7e1dcd02921ab7da38932601
Vincent Ahrend <mail@vincentahrend.com>
2018-09-03 16:08:35 +0200

    Add scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+mkdir ~/displaybot
+mkdir ~/displaybot/clips
+
+echo Please enter the Telegram API token for your displaybot: 
+
+read API_TOKEN
+
+echo $API_TOKEN > ~/displaybot/TELEGRAM_API_TOKEN
+
